16307f610621acc27c89b23b1f03e85c5b4289f7
Aleksander <smlwmy@gmail.com>
2021-02-23 21:13:06 +0500

    add selected item styles

--- select/select.js
+++ select/select.js
@@ -68,8 +68,14 @@ export class Select {
 
   select(id) {
     this.selectedId = id
-
     this.$value.textContent = this.current.value
+
+    this.$el.querySelectorAll('[data-type="item"]').forEach(el => {
+      el.classList.remove('selected')
+    });
+    this.$el.querySelector(`[data-id="${id}"]`).classList.add('selected')
+
+    this.close()
   }
 
   open() {
